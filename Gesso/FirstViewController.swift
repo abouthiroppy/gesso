ed6403ddb6b17a2c0d06d8eaaed90b2c8c16cdae
abouthiroppy <git@about-hiroppy.com>
2015-05-20 21:20:25 +0900

    initial commit

---
+++ Gesso/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Gesso
+//
+//  Created by 廣戸 裕大 on 2015/05/20.
+//  Copyright (c) 2015年 Yuta Hiroto. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
